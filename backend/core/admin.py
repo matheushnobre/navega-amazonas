120a4ff2058492c647fd7e958bdf3776acbff6c6
Matheus <matheus1204h@gmail.com>
2025-11-20 17:36:26 -0300

    register admins

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import City, Harbor
 
-# Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "state", "active")
+    search_fields = ("name", "state")
+    list_filter = ("state", "active")
+
+@admin.register(Harbor)
+class HarborAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "active")
+    search_fields = ("name", "city__name")
+    list_filter = ("city", "active")
