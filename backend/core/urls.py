48608c2ec892dee491f36c629c30987e9c9bd02e
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2025-12-02 19:02:25 -0400

    correção no backend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import UserViewSet, CityViewSet, HarborViewSet, EnterpriseViewSet, TripViewSet, VesselViewSet
+from .viewsets import UserViewSet, CityViewSet, HarborViewSet, EnterpriseViewSet, TripViewSet, VesselViewSet, TripStopViewSet, TripSegmentViewSet
 
 router = DefaultRouter()
 router.register('cities', CityViewSet, basename="city")
@@ -9,6 +9,8 @@ router.register('enterprises', EnterpriseViewSet, basename='enterprise')
 router.register('users', UserViewSet, basename='user')
 router.register('vessels', VesselViewSet, basename='vessel')
 router.register('trips', TripViewSet, basename='trip')
+router.register('trip_stops', TripStopViewSet, basename='trip_stops')
+router.register('trip_segments', TripSegmentViewSet, basename='trip_segments')
 
 urlpatterns = [
     path("api/", include(router.urls)),
