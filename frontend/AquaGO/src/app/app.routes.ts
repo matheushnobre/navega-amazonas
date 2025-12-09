d891a1c2f8b3314da850c73f3e207c700d967bf6
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2025-12-09 17:46:22 -0400

    inserindo seguraça das rotas

--- frontend/AquaGO/src/app/app.routes.ts
+++ frontend/AquaGO/src/app/app.routes.ts
@@ -13,23 +13,25 @@ import { EditVessel } from './features/edit/edit-vessel/edit-vessel';
 import { Tickets } from './features/tickets/tickets';
 import { Vessel } from './features/vessel/vessel';
 import { RegisterTripSegment } from './features/register/register-trip-segment/register-trip-segment';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:Home},
     {path:'login',component:Login},
+
     {path:'edit-enterprise/:id',component:EditEnterprise},
     {path:'edit-vessel/:id',component:EditVessel},
     
-    {path:'register-enterprise',component:RegisterEnterprise},
-    {path:'register-vessel/:id',component:RegisterVessel},
-    {path:'register-trip/:id',component:RegisterTrip},
-    {path:'register-trip-segment/:id',component:RegisterTripSegment},
+    {path:'register-enterprise',canActivate:[authGuard], component:RegisterEnterprise},
+    {path:'register-vessel/:id',canActivate:[authGuard],component:RegisterVessel},
+    {path:'register-trip/:id',canActivate:[authGuard],component:RegisterTrip},
+    {path:'register-trip-segment/:id',canActivate:[authGuard],component:RegisterTripSegment},
 
-    {path:'dashboard',component:Dashboard},
-    {path:'enterprise/:id',component:Enterprise},
-    {path:'vessel/:id',component:Vessel},
+    {path:'dashboard',canActivate:[authGuard],component:Dashboard},
+    {path:'enterprise/:id',canActivate:[authGuard],component:Enterprise},
+    {path:'vessel/:id',canActivate:[authGuard],component:Vessel},
     
-    {path: 'checkout/:id', component:Checkout},
-    {path: 'tickets', component:Tickets}
+    {path: 'checkout/:id',canActivate:[authGuard], component:Checkout},
+    {path: 'tickets',canActivate:[authGuard], component:Tickets}
 ];
